77cceef84f923eeb89c5bb0b593ee221ef5164c9
gwalee <gwalee123@gmail.com>
2021-05-03 16:53:18 +0800

    更改架構

--- gulpfile.js
+++ gulpfile.js
@@ -15,10 +15,9 @@ const cleanCSS = require('gulp-clean-css');
 function sassStyle() {
     return src('dev/sass/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(cleanCSS({compatibility: 'ie10'})) 
+        .pipe(cleanCSS({ compatibility: 'ie10' }))
         .pipe(dest('dist/css'));
 };
-
 
 
 
@@ -35,11 +34,50 @@ function includeHTML() {
         .pipe(dest('dist'));
 }
 
+
+// 打包圖片  // 路徑  dev/images/*.*
+const imagemin = require('gulp-imagemin');
+
+function imgs_dev() {
+    return src(['dev/images/*.*', 'dev/images/**/*.*'])
+        .pipe(dest('dist/img'))
+}
+
+// 壓圖
+function imgs_prod() {
+    return src(['dev/images/*.*', 'dev/images/**/*.*'])
+        .pipe(imagemin())  // 壓圖
+        .pipe(dest('dist/images'))
+}
+
+
+
+const clean = require('gulp-clean');
+
+function clear() {
+    return src('dist', { read: false, allowEmpty: true })
+        .pipe(clean({ force: true }));
+}
+
+
+// js es6 -> es5
+const babel = require('gulp-babel');
+
+function babel5() {
+    return src('dev/js/*.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(dest('dist/js'));
+}
+
+
+// 同步瀏覽器
 const browserSync = require('browser-sync');
 const reload = browserSync.reload;
 
 
-exports.default =  function browser() {
+function browser() {
     browserSync.init({
         server: {
             baseDir: "./dist",
@@ -47,6 +85,17 @@ exports.default =  function browser() {
         },
         port: 3000
     });
-    watch(['dev/*.html' , 'dev/**/*.html'], includeHTML).on('change' , reload);
-    watch(['dev/sass/*.scss' ,'dev/sass/**/*.scss'] , sassStyle).on('change' , reload);
+    watch(['dev/*.html', 'dev/**/*.html'], includeHTML).on('change', reload);
+    watch(['dev/sass/*.scss', 'dev/sass/**/*.scss'], sassStyle).on('change', reload);
+    watch(['dev/images/*.*', 'dev/images/**/*.*'], imgs_dev).on('change', reload);
+    watch('dev/js/*.js', babel5).on('change', reload);
 }
+
+
+// 開發用
+exports.default = series(imgs_dev ,includeHTML , sassStyle  , babel5 , browser)
+
+
+
+// 打包上線用
+exports.prod = series(clear, parallel(includeHTML, sassStyle ,babel5), imgs_prod);
