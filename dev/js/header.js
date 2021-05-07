f9ebd0743fa2af0d18a7b969526678151fbffbfc
rojorojo1027 <rojorojo1027@gmail.com>
2021-05-07 11:07:39 +0800

    修改header.js、footer

--- dev/js/header.js
+++ dev/js/header.js
@@ -1,11 +1,14 @@
-// hamburger btn
-$("button.burgerbtn").on("click", function(e){
-    e.preventDefault();
-    $("div.overlaymenu").fadeIn();
-});
+function doFirst(){
+    // hamburger btn
+    $("button.burgerbtn").on("click", function (e) {
+      e.preventDefault();
+      $("div.overlaymenu").fadeIn();
+    }); 
 
-// 關閉popup
-$("a.closebtn").on("click", function(e){
-    e.preventDefault();
-    $("div.overlaymenu").fadeOut();
-});
+    // 關閉popup
+    $("a.closebtn").on("click", function (e) {
+      e.preventDefault();
+      $("div.overlaymenu").fadeOut();
+    });
+}window.addEventListener('load',doFirst);
+    
