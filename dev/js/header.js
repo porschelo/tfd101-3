1dccb8b6db5a62683f02a523b4317a4c1f2bb248
frankhsu519 <gary80519@yahoo.com.tw>
2021-05-13 18:42:51 +0800

    登入/註冊 popup 完成 剩下樣式修改

--- dev/js/header.js
+++ dev/js/header.js
@@ -10,5 +10,35 @@ function doFirst(){
       e.preventDefault();
       $("div.overlaymenu").fadeOut();
     });
+    //登入popup
+    $("a.login").on("click", function (e) {
+      e.preventDefault();
+      $("div.loginpopup").fadeIn();
+    });
+    
+    $("a.close_login_btn").on("click", function (e) {
+      e.preventDefault();
+      $("div.loginpopup").fadeOut();
+    });
+
+    //註冊popup
+
+    $("a.login").on("click", function (e) {
+      e.preventDefault();
+      $("div.loginpopup").fadeIn();
+    });
+    
+    $("a.gosignup").on("click", function (e) {
+      e.preventDefault();
+      $("div.loginpopup").fadeOut();
+      $("div.signup").fadeIn();
+    });
+
+    $("a.close_signup_btn").on("click", function (e) {
+      e.preventDefault();
+      $("div.signup").fadeOut();
+    });
+
+
 }window.addEventListener('load',doFirst);
     
